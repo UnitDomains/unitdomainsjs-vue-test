40683f3870d510f524c9b424d51417ee5048b794
unit.domains <unit.domains@gmail.com>
2022-10-24 04:12:40 +0800

    add sameple codes.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,8 +14,10 @@ import SupportIcon from "./icons/IconSupport.vue";
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    UnitDomains javascript lib’s
+    <a href="https://docs.unit.domains/dapp/" target="_blank" rel="noopener"
+      >official documentation</a
+    >
     provides you with all information you need to get started.
   </WelcomeItem>
 
@@ -23,69 +25,23 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
+    <template #heading>Sample Code</template>
+    <div>
+      <pre>
+      <code>
+if (window && typeof window.ethereum !== "undefined") {
+  const unitdomains = await UnitDomains.create(window.ethereum);
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener"
-      >Vite</a
-    >. The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener"
-      >Volar</a
-    >. If you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"
-      >Vue Test Utils</a
-    >, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"
-      >Vue Dev Tools</a
-    >. If you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"
-      >Awesome Vue</a
-    >
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You
-    can help us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"
-      >becoming a sponsor</a
-    >.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You
-    can help us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"
-      >becoming a sponsor</a
-    >.
+  console.log(await unitdomains.addr("hello.cat"));
+  console.log(await unitdomains.owner("hello.cat"));
+  console.log(await unitdomains.resolver("hello.cat"));
+  console.log(await unitdomains.ttl("hello.cat"));
+  console.log(await unitdomains.available("hello.cat"));
+  console.log(await unitdomains.rentPrice("hello.cat", 1));
+  console.log(await unitdomains.registerPrice("hello.cat"));
+}
+</code>
+    </pre>
+    </div>
   </WelcomeItem>
 </template>
